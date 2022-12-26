3b5f41e1c4bc724ff080bf5146f393e1efbb1811
gleb-erokhin <gleb.erokhin@gmail.com>
2022-12-26 21:42:51 +0300

    команда считает массив и выводит счетчик с элементами

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿string[] str = {"ghj", "12", "55555", "6"};
+
+    int count = 0;
+    for (int i = 0; i < str.Length; i++)
+    {
+        if(str[i].Length <= 3) 
+        {
+            Console.WriteLine ($"{str[i]}");
+            count++;
+            Console.WriteLine($"count = {count}");
+        }
+
+    }
