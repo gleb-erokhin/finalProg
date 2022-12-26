87627d038475826be94830c85a26eb0e82eaaf23
gleb-erokhin <gleb.erokhin@gmail.com>
2022-12-26 22:02:59 +0300

    остановился на void методе, он выдает строки значений которые удовлетворяют условию

--- Program.cs
+++ Program.cs
@@ -1,11 +1,18 @@
-﻿string[] str = {"ghj", "12", "55555", "6"};
-    int count = 0;
+﻿void Array()
+{
+    string[] str = {"ghj", "12", "55555", "6"};
     for (int i = 0; i < str.Length; i++)
     {
         if(str[i].Length <= 3) 
         {
             Console.WriteLine ($"{str[i]}");
-            count++;
-            Console.WriteLine($"count = {count}");
         }
     }
+}
+
+// string[] PrintArray(string a)
+// {
+    
+//     for(int i = 0; i < )
+// }
+Array();
