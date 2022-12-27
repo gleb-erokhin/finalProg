e93fe87a7e28ccfcc0fc616c4df947929fb4eb9f
gleb.erokhin <gleb.erokhin@gmail.com>
2022-12-27 10:07:05 +0300

    обновленнный код с наполнением нового массива

--- Program.cs
+++ Program.cs
@@ -1,13 +1,26 @@
-﻿string[] str = {"ghrrj", "12rr", "55555", "6rrr", "kjkjkj", "drr", "hirr"};
-// int count = 0;
+﻿string[] str = {"ghrrj", "12rr", "55555", "6rrr", "kjkjkj", "drrb", "hrrb"};
+int count = 0;
 for (int i = 0; i < str.Length; i++)
 {
-    if(str[i].Length <= 3) 
+    if(str[i].Length <= 3) count++; else continue;
+} 
+
+string[] result = new string[count];
+int index = 0;
+foreach(var s in str)
+{
+    if(s.Length <= 3) 
     {
-        string[] res = str[i].Split(",");
-        for(int j =0; j < res.Length; j++)
-        {
-            Console.Write($" res = {res[j]}, j = {j}");
-        }
-    }
+        result[index] = s;
+        index++;
+    }    
+    else continue;
 }
+
+foreach(var el in result)
+{
+    Console.Write($"{el}, ");
+}
+
+
+    
