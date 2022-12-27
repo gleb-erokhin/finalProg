a07481fda1bb426bfaf21cbce4f5eb36be7686bc
gleb-erokhin <gleb.erokhin@gmail.com>
2022-12-27 06:47:10 +0300

    решение выполнено через Split

--- Program.cs
+++ Program.cs
@@ -1,45 +1,13 @@
-﻿string[] Array()
+﻿string[] str = {"ghrrj", "12rr", "55555", "6rrr", "kjkjkj", "drr", "hirr"};
+// int count = 0;
+for (int i = 0; i < str.Length; i++)
 {
-    string[] str = {"ghj", "12", "55555", "6"};
-    int count = 0;
-    string result = "";
-    for (int i = 0; i < str.Length; i++)
+    if(str[i].Length <= 3) 
     {
-        if(str[i].Length <= 3) 
+        string[] res = str[i].Split(",");
+        for(int j =0; j < res.Length; j++)
         {
-            // Console.WriteLine ($"{str[i]}");
-            count++;
-            result = result + str[i];
-
+            Console.Write($" res = {res[j]}, j = {j}");
         }
-        
-    }
-    string[] res = new string[count];
-    for(int j = 0; j < res.Length; j++)
-    {
-        res[j] = str[count];
-    }
-    return res;
-}
-
-
-string[] CreateArray(string[] a)
-{
-
-}
-
-void PrintArray(string[] a)
-{
-    
-    for(int i = 0; i < a.Length; i++)
-    {
-        Console.Write($"{a[i]}, ");
     }
 }
-
-
-
-string[] x = Array();
-PrintArray(x);
-
-
