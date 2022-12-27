6f7192e47a81f68777ccd25e795ebc8df5039342
gleb.erokhin <gleb.erokhin@gmail.com>
2022-12-27 10:36:39 +0300

    готово, созадю методы

--- Program.cs
+++ Program.cs
@@ -1,10 +1,9 @@
-﻿string[] str = {"ghrrj", "12rr", "55555", "6rrr", "kjkjkj", "drrb", "hrrb"};
+﻿string[] str = {"ghrrj", "12rr", "55555", "6rrr", "kjkjkj", "drb", "hrb"};
 int count = 0;
 for (int i = 0; i < str.Length; i++)
 {
     if(str[i].Length <= 3) count++; else continue;
 } 
-
 string[] result = new string[count];
 int index = 0;
 foreach(var s in str)
@@ -20,7 +19,7 @@ foreach(var s in str)
 
 for(int k = 0; k < result.Length; k++)
 {
-    if(k == result.Length -1) Console.Write($"{result[k]}");
+    if(k == result.Length - 1) Console.Write($"{result[k]}");
     else Console.Write($"{result[k]}, ");
 }
 // foreach(var el in result)
